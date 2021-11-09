eb2db4f66100e37ad5e22a90b3c4245885ed0f23
PeachScript <scdzwyxst@gmail.com>
2021-11-09 19:29:14 +0800

    refactor(preset): skip symlink & alias in integrated mode

--- packages/preset-dumi/src/plugins/features/symlink.ts
+++ packages/preset-dumi/src/plugins/features/symlink.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import type { IApi } from '@umijs/types';
+import ctx from '../../context';
 import getHostPkgAlias from '../../utils/getHostPkgAlias';
 import symlink from '../../utils/symlink';
 
@@ -8,67 +9,73 @@ import symlink from '../../utils/symlink';
  * plugin for create node_modules symlink & webpack alias for local packages
  */
 export default (api: IApi) => {
-  const hostPkgAlias = getHostPkgAlias(api.paths).filter(([pkgName]) => pkgName);
+  if (!ctx.opts.isIntegrate) {
+    const hostPkgAlias = getHostPkgAlias(api.paths).filter(([pkgName]) => pkgName);
 
-  /* istanbul ignore if */
-  if (hostPkgAlias.length === 1 && hostPkgAlias[0][0] === 'dumi') {
-    // exit if this project called dumi, will cause import x from 'dumi' not work
-    api.logger.error('[dumi]: cannot start, detected this project is named \'dumi\', please rename it in package.json file.');
-    process.exit(1);
-  }
-
-  // create symlink for packages
-  hostPkgAlias.forEach(([pkgName, pkgPath]) => {
-    const linkPath = path.join(api.paths.cwd, 'node_modules', pkgName);
-
-    // link current pkgs into node_modules, for module resolve in editor
-    if (
-      !fs.existsSync(linkPath) ||
-      (
-        fs.lstatSync(linkPath).isSymbolicLink() &&
-        path.resolve(path.dirname(linkPath), fs.readlinkSync(linkPath)) !== pkgPath
-      )
-    ) {
-      api.utils.rimraf.sync(linkPath);
-      symlink(pkgPath, linkPath);
+    /* istanbul ignore if */
+    if (hostPkgAlias.length === 1 && hostPkgAlias[0][0] === 'dumi') {
+      // exit if this project called dumi, will cause import x from 'dumi' not work
+      api.logger.error('[dumi]: cannot start, detected this project is named \'dumi\', please rename it in package.json file.');
+      process.exit(1);
     }
-  });
 
-  // create webpack alias to src directory for packages
-  api.chainWebpack(config => {
+    // create symlink for packages
     hostPkgAlias.forEach(([pkgName, pkgPath]) => {
-      let srcModule: any;
-      const srcPath = path.join(pkgPath, 'src');
+      const linkPath = path.join(api.paths.cwd, 'node_modules', pkgName);
 
-      try {
-        srcModule = require(srcPath);
-      } catch (err) {
-        if (err.code !== 'MODULE_NOT_FOUND') {
-          srcModule = true;
-        }
-      }
-
-      // use src path instead of main field in package.json if exists
-      if (srcModule) {
-        // exclude es & lib folder
-        if (!config.resolve.alias.has(`${pkgName}/es`)) {
-          config.resolve.alias.set(`${pkgName}/es`, srcPath);
-        }
-
-        if (!config.resolve.alias.has(`${pkgName}/lib`)) {
-          config.resolve.alias.set(`${pkgName}/lib`, srcPath);
-        }
-
-        if (!config.resolve.alias.has(pkgName)) {
-          config.resolve.alias.set(`${pkgName}/src`, srcPath);
-          config.resolve.alias.set(pkgName, srcPath);
-        }
-      } else if (!config.resolve.alias.has(pkgName)) {
-        config.resolve.alias.set(pkgName, pkgPath);
+      // link current pkgs into node_modules, for module resolve in editor
+      if (
+        !fs.existsSync(linkPath) ||
+        (
+          fs.lstatSync(linkPath).isSymbolicLink() &&
+          path.resolve(path.dirname(linkPath), fs.readlinkSync(linkPath)) !== pkgPath
+        )
+      ) {
+        api.utils.rimraf.sync(linkPath);
+        symlink(pkgPath, linkPath);
       }
     });
 
-    // set to umi to be able to use @umijs/preset-dumi alone
+    // create webpack alias to src directory for packages
+    api.chainWebpack(config => {
+      hostPkgAlias.forEach(([pkgName, pkgPath]) => {
+        let srcModule: any;
+        const srcPath = path.join(pkgPath, 'src');
+
+        try {
+          srcModule = require(srcPath);
+        } catch (err) {
+          if (err.code !== 'MODULE_NOT_FOUND') {
+            srcModule = true;
+          }
+        }
+
+        // use src path instead of main field in package.json if exists
+        if (srcModule) {
+          // exclude es & lib folder
+          if (!config.resolve.alias.has(`${pkgName}/es`)) {
+            config.resolve.alias.set(`${pkgName}/es`, srcPath);
+          }
+
+          if (!config.resolve.alias.has(`${pkgName}/lib`)) {
+            config.resolve.alias.set(`${pkgName}/lib`, srcPath);
+          }
+
+          if (!config.resolve.alias.has(pkgName)) {
+            config.resolve.alias.set(`${pkgName}/src`, srcPath);
+            config.resolve.alias.set(pkgName, srcPath);
+          }
+        } else if (!config.resolve.alias.has(pkgName)) {
+          config.resolve.alias.set(pkgName, pkgPath);
+        }
+      });
+
+      return config;
+    });
+  }
+
+  // set to umi to be able to use @umijs/preset-dumi alone
+  api.chainWebpack(config => {
     config.resolve.alias.set('dumi', process.env.UMI_DIR);
 
     return config;
